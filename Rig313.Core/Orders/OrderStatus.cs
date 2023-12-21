d9c4ae6f0cbb653c7bb289051aa2c21d692f35b5
Md. Taufik Hasan <taufik.cse.bd@gmail.com>
2023-12-21 12:12:29 +0600

    added order status enum

---
+++ Rig313.Core/Orders/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rig313.Core.Orders
+{
+	public enum OrderStatus
+	{
+		New = 1,
+		Processing = 2,
+		Completed = 3,
+		Canceled = 4
+	}
+}
