608b5bd7f7222e58c8df443abc7ad2221cd2a829
emran <emranbspt@gmail.com>
2023-12-22 17:04:22 +0600

    add view

--- Rig313.Core/Orders/Order.cs
+++ Rig313.Core/Orders/Order.cs
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-﻿using System;
-=======
-﻿using Rig313.Core.Customers;
 using System;
->>>>>>> Stashed changes
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +9,8 @@ namespace Rig313.Core.Orders
     public class Order:BaseEntity
     {
         public int CustomerId { get; set; }
-<<<<<<< Updated upstream
         public String  ShipmentAddress { get; set; }
         public int OrderStatus { get; set; }
-=======
-        public string  ShipmentAddress { get; set; }
-        public int OrderStatusId { get; set; }
->>>>>>> Stashed changes
         public double TotalPrice { get; set; }
         public int PaymentMethod { get; set; }
         public int PaymentStatus { get; set; }
@@ -33,12 +23,5 @@ namespace Rig313.Core.Orders
         public DateTime? CancelDateTime { get; set; }
         public DateTime? ReturnDateTime { get; set; }
         public DateTime? PaymentDateTime { get; set; }
-<<<<<<< Updated upstream
-=======
-
-        public Customer? Customer { get; private set; }
-        public IList<OrderItem>? OrderItems { get; private set; }
-
->>>>>>> Stashed changes
     }
 }
--- Rig313.Web/Program.cs
+++ Rig313.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Shop}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
