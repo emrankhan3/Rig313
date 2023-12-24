d4014114b972d944d58036d54315a182b8a9ed33
emran <emranbspt@gmail.com>
2023-12-24 12:54:38 +0600

    add Irepository, contrubuted by Monjurul, Muntasir

---
+++ Rig313.Data/DataAccess/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rig313.Core.Carting;
+using Rig313.Core.Categories;
+using Rig313.Core.Customers;
+using Rig313.Core.Inventories;
+using Rig313.Core.Orders;
+using Rig313.Core.Products;
+using Rig313.Core.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rig313.Data.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Inventory> Inventory{ get; set; }
+        public DbSet<Order> Order  { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<User> Users{ get; set; }
+        public DbSet<UserPermission> UserPermissions{ get; set; }
+        public DbSet<Customer> Customer  { get; set; }
+        public DbSet<Cart> Cart{ get; set; }
+        public DbSet<CartItem> CartItem{ get; set; }
+
+    }
+}
---
+++ Rig313.Data/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Rig313.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rig313.Data.IRepository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public T? GetById(int id);
+        public IEnumerable<T?> GetAll();
+        public void Add(T entity);
+
+        public void Update(T entity);
+        public void Delete(T entity);
+
+    }
+}
